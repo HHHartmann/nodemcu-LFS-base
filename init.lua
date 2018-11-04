3804e4e4832c56e363117f4e9687b07236f78dff
Gregor <HHHartmann@users.noreply.github.com>
2018-11-04 13:17:00 +0100

    Revert "do not commit actual config"
    This reverts commit 223ca21b9f7909603ba545b02036a366dea7fee9.

--- init.lua
+++ init.lua
@@ -1,13 +1,5 @@
 
 print('init.lua')
-	gpio.mode(1,gpio.OUTPUT)
-	gpio.write(1,gpio.LOW)
-	gpio.mode(2,gpio.OUTPUT)
-	gpio.write(2,gpio.LOW)
-	gpio.mode(3,gpio.OUTPUT)
-	gpio.write(3,gpio.LOW)
-	gpio.mode(4,gpio.OUTPUT)
-	gpio.write(4,gpio.LOW)
 print('heap: ',node.heap(),(function() collectgarbage() return node.heap() end) ())
 
 
